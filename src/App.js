0e4c870aa40e39b0937573f06bcfcbc9bd0a20d7
George Yiakoumi <George@Georges-MacBook-Pro-2.local>
2024-11-15 19:47:55 +0000

    adding the build

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
 import NFTGenerator from './pages/NFTGenerator';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/nft-generator" element={<NFTGenerator />} />
-      </Routes>
-    </Router>
+    <div>
+      <NFTGenerator />
+    </div>
   );
 }
 
---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const collections = [
+    { name: 'Bored Ape Yacht Club', address: '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D' },
+    { name: 'Mutant Ape Yacht Club', address: '0x60E4d786628Fea6478F785A6d7e704777c86a7c6' },
+  ];
+  
+  export const specialMutantApeTokens = [
+    "4318", "9209", "30004", "30003", "30006", "1796", "30001", "4849", "30002", "6957", "30005", "30000"
+  ];
+  
