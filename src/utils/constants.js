a981b107ad406b08895df49b43c1f7118d263495
George Yiakoumi <gykmi91@protonmail.com>
2024-11-20 00:32:27 +0000

    disabling gg for now

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const collections = [
     { name: 'Bored Ape Yacht Club', address: '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D', chain: 'eth-mainnet' },
     { name: 'Mutant Ape Yacht Club', address: '0x60E4d786628Fea6478F785A6d7e704777c86a7c6', chain: 'eth-mainnet' },
-    { name: 'Gutter Gang', address: '0xEdB61f74B0d09B2558F1eeb79B247c1F363Ae452', chain: 'eth-mainnet' },
+   // { name: 'Gutter Gang', address: '0xEdB61f74B0d09B2558F1eeb79B247c1F363Ae452', chain: 'eth-mainnet' },
     
   ];
   
